9ff0033d201ecb376b10db5931c706c8e5296ab7
Juozas Kasiliauskas <juozas@unity3d.com>
2019-11-20 15:13:23 +0200

    Reordering script locations

---
+++ Assets/Scenes/8. TL - Activation Track/ActivationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ActivationController : MonoBehaviour
+{
+
+    public PlayableDirector timeline;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Restart()
+    {
+        timeline.Stop();
+        timeline.Play();
+    }
+}
